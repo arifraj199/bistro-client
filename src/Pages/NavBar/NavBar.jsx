f4d59b3356e8d57c20c1386568fd90d95a688401
arifraj199 <karif9514@gmail.com>
2023-10-04 14:18:41 +0600

    homepage banner added

--- src/Pages/NavBar/NavBar.jsx
+++ src/Pages/NavBar/NavBar.jsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    const navOptions = <>
-    <li><Link to='/'>Home</Link></li>
-    <li><Link to='/contact'>Contact Us</Link></li>
-    <li><Link to='/dashboard'>Dashboard</Link></li>
-    <li><Link to='ourMenu'>Our Menu</Link></li>
-    <li><Link to='outShop'>Our Shop</Link></li>
+  const navOptions = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact Us</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="ourMenu">Our Menu</Link>
+      </li>
+      <li>
+        <Link to="outShop">Our Shop</Link>
+      </li>
     </>
+  );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar fixed z-10 bg-opacity-25 bg-black text-white max-w-screen-xl">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -33,15 +45,13 @@ const NavBar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-                {navOptions}
+              {navOptions}
             </ul>
           </div>
           <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            {navOptions}
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
